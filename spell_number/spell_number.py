80d73facba64d7db27e09dab74aaa16bf69d936d
Noel Vincent <noelvincent@me.com>
2024-07-07 12:40:58 +0900

    Added number format validation and corrected a serious error with the digit_length method that was yeilding incorrect lengths for exact powers of 10

--- spell_number/spell_number.py
+++ spell_number/spell_number.py
@@ -4,129 +4,145 @@ import re
 
 class SpellNumber:
 
-	def __init__(self, number):
-		self.spelling = self.spell_number(number)
+    def __init__(self, number):
+        if SpellNumber.is_valid_number(number):
+            self.spelling = self.spell_number(SpellNumber.cast_to_number(number))
+        else:
+            self.spelling = None
 
-	def __str__(self):
-		return f"{self.spelling}"
+    def __str__(self):
+        return f"{self.spelling}"
 
-	@staticmethod
-	def initialize_spelling_hash():
-		spelling_hash = {}
+    @staticmethod
+    def is_valid_number(num):
+        return True if re.search(r"\-?\d*\.?\d+", str(num)) else False
 
-		ones_and_teens = "zero,one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve,thirteen,fourteen,fifteen,sixteen,seventeen,eighteen,nineteen".split(",")
-		two_digits = "twenty,thirty,forty,fifty,sixty,seventy,eighty,ninety".split(",")
+    @staticmethod
+    def cast_to_number(num):
+        if type(num) == float or type(num) == int:
+            return num
+        else:
+            num = str(num)
+            return float(num) if "." in num else int(num)
 
-		for i,spelling in enumerate(ones_and_teens):
-			spelling_hash[i] = spelling
+    @staticmethod
+    def initialize_spelling_hash():
+        spelling_hash = {}
 
-		for i,spelling in enumerate(two_digits):
-			spelling_hash[(i+2)*10] = spelling
+        ones_and_teens = "zero,one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve,thirteen,fourteen,fifteen,sixteen,seventeen,eighteen,nineteen".split(",")
+        two_digits = "twenty,thirty,forty,fifty,sixty,seventy,eighty,ninety".split(",")
 
-		return spelling_hash
+        for i,spelling in enumerate(ones_and_teens):
+            spelling_hash[i] = spelling
 
-	@staticmethod
-	def digit_length(num):
-		return math.ceil(math.log(num, 10)) if num != 0 else 1
+        for i,spelling in enumerate(two_digits):
+            spelling_hash[(i+2)*10] = spelling
 
-	@staticmethod
-	def get_digit_at(pos, num):
-		return (num // (10 ** (pos - 1))) % 10
+        return spelling_hash
 
-	@staticmethod
-	def get_digit_range(high, low, num):
-		total = 0
-		for i,pos in enumerate(range(low, high + 1)):
-			total += SpellNumber.get_digit_at(pos, num) * (10 ** (i))
+    @staticmethod
+    def digit_length(num):
+        return math.floor(math.log(num, 10) + 1) if num != 0 else 1
 
-		return total
+    @staticmethod
+    def get_digit_at(pos, num):
+        return (num // (10 ** (pos - 1))) % 10
 
-	@staticmethod
-	def spell_to_999(num_spelling, num):
-		num_string = []
-		ones_place = SpellNumber.get_digit_at(1, num)
+    @staticmethod
+    def get_digit_range(high, low, num):
+        total = 0
+        for i,pos in enumerate(range(low, high + 1)):
+            total += SpellNumber.get_digit_at(pos, num) * (10 ** (i))
 
-		if num > 99:
-			num_string.append(num_spelling[SpellNumber.get_digit_at(3, num)])
-			num_string.append("hundred")
-		if num > 9:
-			tens_place = SpellNumber.get_digit_at(2, num)
+        return total
 
-			if tens_place == 1:
-				num_string.append(num_spelling[(tens_place * 10) + ones_place])
-			else:
-				num_string.append(num_spelling[SpellNumber.get_digit_at(2, num) * 10])
-				if ones_place != 0:
-					num_string.append(num_spelling[SpellNumber.get_digit_at(1, num)])
+    @staticmethod
+    def spell_to_999(num_spelling, num):
+        num_string = []
+        ones_place = SpellNumber.get_digit_at(1, num)
+        if num > 99:
+            num_string.append(num_spelling[SpellNumber.get_digit_at(3, num)])
+            num_string.append("hundred")
+        if num > 9:
+            tens_place = SpellNumber.get_digit_at(2, num)
 
-		else:
-			num_string.append(num_spelling[SpellNumber.get_digit_at(1, num)])
+            if tens_place == 1:
+                num_string.append(num_spelling[(tens_place * 10) + ones_place])
+            else:
+                num_string.append(num_spelling[SpellNumber.get_digit_at(2, num) * 10])
+                if ones_place != 0:
+                    num_string.append(num_spelling[SpellNumber.get_digit_at(1, num)])
 
-		return " ".join(num_string)
+        else:
+            num_string.append(num_spelling[SpellNumber.get_digit_at(1, num)])
 
-	@staticmethod
-	def form_digit_groups(num):
-		digit_len = SpellNumber.digit_length(num)
-		remainder = digit_len - ((digit_len // 3) * 3)
-		digit_groups = []
+        return " ".join(num_string)
 
-		if remainder:
-			digit_groups.append(SpellNumber.get_digit_range(digit_len, digit_len - remainder + 1, num))
+    @staticmethod
+    def form_digit_groups(num):
+        digit_len = SpellNumber.digit_length(num)
+        remainder = digit_len - ((digit_len // 3) * 3)
+        digit_groups = []
 
-		for i in range(digit_len - remainder, 0, -3):
-			digit_groups.append(SpellNumber.get_digit_range(i, i - 2, num))
+        if remainder:
+            digit_groups.append(SpellNumber.get_digit_range(digit_len, digit_len - remainder + 1, num))
 
-		return digit_groups
+        for i in range(digit_len - remainder, 0, -3):
+            digit_groups.append(SpellNumber.get_digit_range(i, i - 2, num))
 
-	@staticmethod
-	def generate_group_names():
-		shortened = "m,b,tr,quadr,quint,sext,sept,oct,non,dec,undec,duodec,tredec,quattuordec,quindec,sexdec,septendec,octodec,novemdec,vigint,unvigint,duovigint,tresvigint,quattuorvigint,quinvigint,sesvigint,septenvigint,octovigint,novemvigint,trigint,untrigint,duotrigint,trestrigint,quattuortrigint,quintrigint,sestrigint,septentrigint,octotrigint,novemtrigint,quadragint,unquadragint,duoquadragint,tresquadragint,quattuorquadragint,quinquadragint,sesquadragint,septenquadragint,octoquadragint,novemquadragint,quinquagint,unquinquagint,duoquinquagint,tresquinquagint,quattuorquinquagint,quinquinquagint,sesquinquagint,septenquinquagint,octoquinquagint,novemquinquagint,sexagint,unsexagint,duosexagint,tresexagint,quattuorsexagint,quinsexagint,sessexagint,septensexagint,octosexagint,novemsexagint,septuagint,unseptuagint,duoseptuagint,treseptuagint,quattuorseptuagint,quinseptuagint,sesseptuagint,septenseptuagint,octoseptuagint,novemseptuagint,octogint,unoctogint,duooctogint,tresoctogint,quattuoroctogint,quinoctogint,sessoctogint,septenoctogint,octooctogint,novemoctogint,nonagint,unnonagint,duononagint,trenonagint,quattuornonagint,quinnonagint,sesnonagint,septennonagint,octononagint,novemnonagint,cent"
-		return ["", "thousand"] + [name + "illion" for name in shortened.split(",")]
+        return digit_groups
 
-	@staticmethod
-	def spell_number(num):
-		num_string = []
+    @staticmethod
+    def generate_group_names():
+        shortened = "m,b,tr,quadr,quint,sext,sept,oct,non,dec,undec,duodec,tredec,quattuordec,quindec,sexdec,septendec,octodec,novemdec,vigint,unvigint,duovigint,tresvigint,quattuorvigint,quinvigint,sesvigint,septenvigint,octovigint,novemvigint,trigint,untrigint,duotrigint,trestrigint,quattuortrigint,quintrigint,sestrigint,septentrigint,octotrigint,novemtrigint,quadragint,unquadragint,duoquadragint,tresquadragint,quattuorquadragint,quinquadragint,sesquadragint,septenquadragint,octoquadragint,novemquadragint,quinquagint,unquinquagint,duoquinquagint,tresquinquagint,quattuorquinquagint,quinquinquagint,sesquinquagint,septenquinquagint,octoquinquagint,novemquinquagint,sexagint,unsexagint,duosexagint,tresexagint,quattuorsexagint,quinsexagint,sessexagint,septensexagint,octosexagint,novemsexagint,septuagint,unseptuagint,duoseptuagint,treseptuagint,quattuorseptuagint,quinseptuagint,sesseptuagint,septenseptuagint,octoseptuagint,novemseptuagint,octogint,unoctogint,duooctogint,tresoctogint,quattuoroctogint,quinoctogint,sessoctogint,septenoctogint,octooctogint,novemoctogint,nonagint,unnonagint,duononagint,trenonagint,quattuornonagint,quinnonagint,sesnonagint,septennonagint,octononagint,novemnonagint,cent"
+        return ["", "thousand"] + [name + "illion" for name in shortened.split(",")]
 
-		if num < 0:
-			num_string.append("negative")
-			num = abs(num)
+    @staticmethod
+    def spell_number(num):
+        num_string = []
 
-		decimal_portion = None
+        if num < 0:
+            num_string.append("negative")
+            num = abs(num)
 
-		if type(num) == float:
-			match = re.search(r"\.\d*", str(num))[0][1:]
-			decimal_portion = [int(ch) for ch in match] if match else None
+        decimal_portion = None
 
-		num_spelling = SpellNumber.initialize_spelling_hash()
+        if type(num) == float:
+            match = re.search(r"\.\d+", str(num))[0][1:]
+            decimal_portion = [int(ch) for ch in match] if match else None
 
-		group_names = SpellNumber.generate_group_names()
+        num_spelling = SpellNumber.initialize_spelling_hash()
 
-		digit_groups = SpellNumber.form_digit_groups(num)
+        group_names = SpellNumber.generate_group_names()
 
-		if len(digit_groups) > len(group_names):
-			return None
+        digit_groups = SpellNumber.form_digit_groups(num)
 
-		while len(digit_groups) > 0:
-			current_group = digit_groups.pop(0)
-			num_string.append(SpellNumber.spell_to_999(num_spelling, current_group))
+        if len(digit_groups) > len(group_names):
+            return None
 
-			if group_names[len(digit_groups)]:
-				num_string.append(group_names[len(digit_groups)])
+        while len(digit_groups) > 0:
+            current_group = digit_groups.pop(0)
+            num_string.append(SpellNumber.spell_to_999(num_spelling, current_group))
 
-		if decimal_portion:
-			num_string.append("point")
-			for dec_place in decimal_portion:
-				num_string.append(num_spelling[dec_place])
+            if group_names[len(digit_groups)]:
+                num_string.append(group_names[len(digit_groups)])
 
-		return " ".join(num_string)
+        if decimal_portion:
+            num_string.append("point")
+            for dec_place in decimal_portion:
+                num_string.append(num_spelling[dec_place])
+
+        return " ".join(num_string)
 
 def main():
     parser = argparse.ArgumentParser(description="Convert numbers to words")
     parser.add_argument('number', type=str, help='The number to convert')
     args = parser.parse_args()
-    number = float(args.number) if '.' in args.number else int(args.number)
-    spell_number_instance = SpellNumber(number)
-    print(spell_number_instance)
+    spell_number_instance = SpellNumber(args.number)
+    if spell_number_instance.spelling:
+        print(spell_number_instance)
+    else:
+        print(f'"{args.number}" is not a valid number.')
 
 if __name__ == "__main__":
     main()
--- tests/test_spell_number.py
+++ tests/test_spell_number.py
@@ -1,7 +1,15 @@
 import pytest
 from spell_number.spell_number import SpellNumber
 
+def test_digit_length():
+    # Check that digit_length method produces the same output as len(string) for positive integers.
+    for i in range(1000):
+        assert SpellNumber.digit_length(i) == len(str(i))
+
 def test_spell_number():
+    for i,pair in enumerate(list(SpellNumber.initialize_spelling_hash().items())[:21]):
+        assert SpellNumber(i).spelling == pair[1]
+
     assert SpellNumber(123).spelling == "one hundred twenty three"
     assert SpellNumber(0).spelling == "zero"
     assert SpellNumber(10001).spelling == "ten thousand one"
